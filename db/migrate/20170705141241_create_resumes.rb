4cfd08943123c3efd9fabf6eeccf82fa2cea6a2f
paul <hammerschmidtpaul@gmail.com>
2017-07-05 14:37:09 -0500

    migrations have columns for initial schema, resumes table doesn't have pdf_file column

--- db/migrate/20170705141241_create_resumes.rb
+++ db/migrate/20170705141241_create_resumes.rb
@@ -1,4 +1,11 @@
 class CreateResumes < ActiveRecord::Migration
   def change
+    create_table :resumes do |t|
+      t.string :file_location, null: false
+
+      t.integer :applicant_id, null: false
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20170705141254_create_applications.rb
+++ db/migrate/20170705141254_create_applications.rb
@@ -1,4 +1,10 @@
 class CreateApplications < ActiveRecord::Migration
   def change
+    create_table :applications do |t|
+      t.integer :resume_id, null: false
+      t.integer :position_id, null: false
+
+      t.timestamps
+    end
   end
 end
