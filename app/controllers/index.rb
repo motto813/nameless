42e0a532e4cfe1e92b6d3a6b01e107f5edfcca0e
paul <hammerschmidtpaul@gmail.com>
2017-07-06 00:20:34 -0500

    looking homepage has login and registration forms for applicant users, empty post routes added to index for logging in

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,6 +6,14 @@ get '/looking' do
   erb :looking
 end
 
+post '/looking/login' do
+  # see user registration DBC challenge
+end
+
 get '/hiring' do
   erb :hiring
+end
+
+post '/hiring/login' do
+  # see user registration DBC challenge
 end
