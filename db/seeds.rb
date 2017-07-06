548d69ee4de6a59a65ea66bd6ab43ad7fad8cebf
paul <hammerschmidtpaul@gmail.com>
2017-07-05 23:11:52 -0500

    seeds file creates 4 rows in each table, currently associates foreign key IDs using .times iterator so only 1..4, would like to make this more robust eventually

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 require 'faker'
 
-number_of_applicants_wanted = 4
+# APPLICATIONS
 
+number_of_applicants_wanted = 4
 new_applicants_needed = number_of_applicants_wanted - Applicant.count
 
 new_applicants_needed.times do
@@ -9,27 +10,76 @@ new_applicants_needed.times do
   applicant[:email] = Faker::Internet.unique.safe_email("#{applicant[:first_name]}.#{applicant[:last_name]}")
   applicant[:password] = "password"
 
-  applicant = User.new(applicant)
+  applicant = Applicant.new(applicant)
   applicant.save!
 end
 
-number_of_recruiters_wanted = 4
+# RECRUITERS
 
+number_of_recruiters_wanted = 4
 new_recruiters_needed = number_of_recruiters_wanted - Recruiter.count
 
 new_recruiters_needed.times do
   recruiter = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name }
-  recruiter[:email] = Faker::Internet.unique.safe_email("#{applicant[:first_name]}.#{applicant[:last_name]}")
+  recruiter[:email] = Faker::Internet.unique.safe_email("#{recruiter[:first_name]}.#{recruiter[:last_name]}")
   recruiter[:password] = "password"
 
   recruiter = Recruiter.new(recruiter)
   recruiter.save!
 end
 
-number_of_resumes_wanted = 4
+# RESUMES
 
+number_of_resumes_wanted = 4
 new_resumes_needed = number_of_resumes_wanted - Resume.count
 
-new_resumes_needed.times do
-  resume = { file_name: Faker::StarWars.planet }
+new_resumes_needed.times do |x|
+  resume = { file_name: Faker::StarWars.planet + ".pdf" }
+  resume = Resume.new(resume)
+
+  resume.applicant_id = x + 1
+
+  resume.save!
+end
+
+# COMPANIES
+
+number_of_companies_wanted = 4
+new_companies_needed = number_of_companies_wanted - Company.count
+
+new_companies_needed.times do
+  company = { name: Faker::Company.name }
+
+  company = Company.new(company)
+
+  company.save!
+end
+
+# POSITIONS
+
+number_of_positions_wanted = 4
+new_positions_needed = number_of_positions_wanted - Position.count
+
+new_positions_needed.times do |x|
+  position = { title: Faker::Job.title, description: Faker::Friends.quote }
+  position = Position.new(position)
+
+  position.company_id = x + 1
+  position.recruiter_id = x + 1
+
+  position.save!
+end
+
+# APPLICATIONS
+
+number_of_applications_wanted = 4
+new_applications_needed = number_of_applications_wanted - Application.count
+
+new_applications_needed.times do |x|
+  application = Application.new
+
+  application.resume_id = x + 1
+  application.position_id = x + 1
+
+  application.save!
 end
