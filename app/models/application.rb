ec14e353f7f866be708a4293abc41121b6feca64
paul <hammerschmidtpaul@gmail.com>
2017-07-07 15:18:14 -0500

    refactored to have instance methods in position and application models for authorization features related to logged in user

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,8 @@ class Application < ActiveRecord::Base
 
   validates :resume_id, presence: true
   validates :position_id, presence: true
+
+  def is_created_by_applicant?(applicant)
+    self.resume.applicant == applicant
+  end
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -8,4 +8,15 @@ class Position < ActiveRecord::Base
   validates :description, presence: true
   validates :company_id, presence: true
   validates :recruiter_id, presence: true
+
+  def is_applied_to_by_applicant?(applicant)
+    self.applications.each do |application|
+      return true if application.resume.applicant == applicant
+    end
+    return false
+  end
+
+  def was_created_by_recruiter?(recruiter)
+    self.recruiter == recruiter
+  end
 end
