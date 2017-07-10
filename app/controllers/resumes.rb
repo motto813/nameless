1aaa5627648c6053b29e5201b503dbc86ab465da
paul <hammerschmidtpaul@gmail.com>
2017-07-10 17:16:26 -0500

    added extension whitelist to ResumeUploader, new resume route saves instance variable with the logged in applicant, new resume form has file input and hidden input for logged in applicant

--- app/controllers/resumes.rb
+++ app/controllers/resumes.rb
@@ -12,42 +12,46 @@ get '/resumes/new' do
   @companies = Company.all
   @recruiters = Recruiter.all
 
+  if session[:user].instance_of? Applicant
+    @applicant = session[:user]
+  end
+
   erb :"resumes/new"
 end
 
 post '/resumes' do
-  @Resume = Resume.new(params[:Resume])
+  @resume = Resume.new(params[:Resume])
 
-  if @Resume.save
+  if @resume.save
     redirect '/resumes'
   else
     @companies = Company.all
     @recruiters = Recruiter.all
 
-    @errors = @Resume.errors.full_messages
+    @errors = @resume.errors.full_messages
     erb :"resumes/new"
   end
 end
 
 get '/resumes/:id' do
-  @Resume = Resume.find(params[:id])
+  @resume = Resume.find(params[:id])
 
   erb :"resumes/show"
 end
 
 get '/resumes/:id/edit' do
-  @Resume = Resume.find(params[:id])
+  @resume = Resume.find(params[:id])
 
   erb :"resumes/edit"
 end
 
 
 put '/resumes/:id' do
-  @Resume = Resume.find(params[:id])
+  @resume = Resume.find(params[:id])
 
-  @Resume.assign_attributes(params[:Resume])
+  @resume.assign_attributes(params[:Resume])
 
-  if @Resume.save
+  if @resume.save
     redirect '/resumes'
   else
     erb :"resumes/edit"
@@ -56,16 +60,15 @@ end
 
 
 delete '/resumes/:id' do
-  @Resume = Resume.find(params[:id])
+  @resume = Resume.find(params[:id])
 
-  @Resume.destroy
+  @resume.destroy
 
   redirect '/resumes'
 end
 
 get '/resumes/download/:file_name' do |file_name|
   resume = Resume.find_by(file_name: file_name)
-  puts resume.class
   unless resume.nil?
     file = resume.resume_file.current_path
     send_file(file, disposition: 'attachment', filename: resume.file_name)
--- app/models/resume_uploader.rb
+++ app/models/resume_uploader.rb
@@ -1,3 +1,5 @@
 class ResumeUploader < CarrierWave::Uploader::Base
   storage :file
+  def extension_whitelist
+    %w( jpg jpeg gif png pdf doc docx txt odt )
 end
