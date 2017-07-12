972b07c9efd9a00e35d07deaa99d80abcb97b07f
paul <hammerschmidtpaul@gmail.com>
2017-07-12 17:04:30 -0500

    password confirmation validations for applicant and recruiter models

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -6,7 +6,9 @@ class Recruiter < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
 
   before_save :encrypt_password
 
