34f2f24a713e0447e320faf2d36e2549df4db923
paul <hammerschmidtpaul@gmail.com>
2017-07-13 14:03:29 -0500

    formatting for interviews edit and show pages

--- app/helpers/interview_form_helpers.rb
+++ app/helpers/interview_form_helpers.rb
@@ -4,6 +4,10 @@ module InterviewFormHelpers
 
     datetime.strftime("%Y-%m-%dT%I:%M")
   end
+
+  def date_and_time(datetime)
+    datetime.strftime("%A %B %e, %Y at %l:%M %P")
+  end
 end
 
 helpers InterviewFormHelpers
